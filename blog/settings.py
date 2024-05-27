23044c64195c708c1b6630568738cde5864d9895
cherish-1201 <heohyeog@heohyeogs-MacBook-Air.local>
2024-05-28 00:40:00 +0900

    Auto commit

--- blog/settings.py
+++ blog/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR/'static']
 
 # 유저가 올리는 정적파일을 media라고 한다.
